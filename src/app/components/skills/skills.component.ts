02df0df83b906281dffca81ec1da754ce736eb90
masumkazibd <masum.kazi.hantmk@gmail.com>
2025-07-04 23:37:36 +0600

    update skill section

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -9,8 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './skills.component.css'
 })
 export class SkillsComponent {
-skills = [
-    'Angular', 'TypeScript', 'JavaScript', 'HTML5', 'CSS3', 'Tailwind CSS',
-    'Node.js', 'Express', 'Python', 'Firebase', 'SQL', 'Git'
+skillCategories = [
+    { title: 'Programming Languages', skills: ['C#', 'JavaScript'] },
+    { title: 'Frameworks', skills: ['MVC.Net', '.Net Core', 'Entity Framework', 'Angular'] },
+    { title: 'Web Application', skills: ['HTML', 'CSS', 'Bootstrap', 'Node.js', 'API', 'Jquery'] },
+    { title: 'Database', skills: ['SQL Server', 'SQL'] },
+    { title: 'Developer Tools', skills: ['Visual Studio', 'SQL Server', 'VsCode', 'Hostinger VPS', 'Android Studio'] },
+    { title: 'Operating Systems', skills: ['Windows', 'Ubuntu', 'Debian', 'Oracle', 'Linux'] },
+    { title: 'Others', skills: ['Jira', 'Confluence', 'Git & GitHub', 'Clean Code', 'WordPress', 'Shopify'] }
   ];
 }
